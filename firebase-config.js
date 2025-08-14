e947b52562b36b5c7121f4633292cbf9bc2a9f9a
potnik1228 <156456263+potnik1228@users.noreply.github.com>
2025-08-14 12:59:04 -0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+window.firebaseConfig = {
+    apiKey: "AIzaSyBcH2sW8OR2IkL5MKWsSvP8VRKROeP__i0",
+    authDomain: "blue-team-cards.firebaseapp.com", 
+    projectId: "blue-team-cards",
+    storageBucket: "blue-team-cards.firebasestorage.app",
+    messagingSenderId: "1002711590432",
+    appId: "1:1002711590432:web:2779726689ce7117bfbe45"
+};
